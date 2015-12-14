98394dbe6e60ac7c7d54848e4e2c70c2f1affd23
d3moid <d3moid@users.noreply.github.com>
2015-12-14 07:26:36 +0200

    Cat Clicker Premium

--- app.js
+++ app.js
@@ -1,19 +1,59 @@
-var $cat1 = document.getElementById('cat1'),
-$clicks1 = document.getElementById('clicks1'), clickCounter1 = 0,
-cat1name = 'Stalin', $cat1name = document.getElementById('cat1-name');
-$cat1name.innerHTML = cat1name;
+$(function() {
+	var cats = [
+		{
+			name: 'Mojo',
+			clicks: 0,
+			image: 'images/cat1.jpg'
+		},
+		{
+			name: 'Stalin',
+			clicks: 0,
+			image: 'images/cat2.jpg'
+		},
+		{
+			name: 'Lenin',
+			clicks: 0,
+			image: 'images/cat3.jpg'
+		}
+	];
 
-var $cat2 = document.getElementById('cat2'),
-$clicks2 = document.getElementById('clicks2'), clickCounter2 = 0,
-cat2name = 'Lenin', $cat2name = document.getElementById('cat2-name');
-$cat2name.innerHTML = cat2name;
+	var $catList = $('#cat-list');
+	cats.forEach(function(cat) {
+		$catList.append('<li><button id="' + cat.name + '">' + cat.name + '</button></li>');
+	});
 
-$cat1.addEventListener('click', function() {
-	clickCounter1++;
-	$clicks1.innerHTML = clickCounter1;
-}, false);
+	var $catName = $('#cat-name');
+	var $catClicks = $('#cat-clicks');
+	var $catPics = $('.cat-pics');
+	cats.forEach(function(cat) {
+		$('#' + cat.name).click(function() {
+			loadCat(cat);
+		});
+	});
 
-$cat2.addEventListener('click', function() {
-	clickCounter2++;
-	$clicks2.innerHTML = clickCounter2;
-}, false); 
+	$catPics.click(function() {
+		var catName = $('#cat-name').text()
+		var currentCat = getCat(catName)
+		currentCat.clicks++;
+		$catClicks.text(currentCat.clicks);
+	});
+
+	function getCat(catName) {
+		var len = cats.length, i;
+		for (i = 0; i < len; i++) {
+			if (cats[i].name === catName) {
+				return cats[i];
+			}
+		}
+	}
+
+	function loadCat(cat) {
+		$catName.text(cat.name);
+		$catClicks.text(cat.clicks);
+		$catPics.attr("src", cat.image);
+	}
+
+	loadCat(cats[0]);
+	
+});
+
