c1f9bc6daa94ea75bd0630b226633693fe661cd4
d3moid <d3moid@users.noreply.github.com>
2015-12-16 12:24:24 +0200

    feat: Refactored with separation of concerns (MVO).

--- app.js
+++ app.js
@@ -1,59 +1,98 @@
 $(function() {
-	var cats = [
-		{
-			name: 'Mojo',
-			clicks: 0,
-			image: 'images/cat1.jpg'
+
+	var model = {
+		init: function(cat) {
+			this.currentCat = cat;
 		},
-		{
-			name: 'Stalin',
-			clicks: 0,
-			image: 'images/cat2.jpg'
-		},
-		{
-			name: 'Lenin',
-			clicks: 0,
-			image: 'images/cat3.jpg'
-		}
-	];
-
-	var $catList = $('#cat-list');
-	cats.forEach(function(cat) {
-		$catList.append('<li><button id="' + cat.name + '">' + cat.name + '</button></li>');
-	});
-
-	var $catName = $('#cat-name');
-	var $catClicks = $('#cat-clicks');
-	var $catPics = $('.cat-pics');
-	cats.forEach(function(cat) {
-		$('#' + cat.name).click(function() {
-			loadCat(cat);
-		});
-	});
-
-	$catPics.click(function() {
-		var catName = $('#cat-name').text()
-		var currentCat = getCat(catName)
-		currentCat.clicks++;
-		$catClicks.text(currentCat.clicks);
-	});
-
-	function getCat(catName) {
-		var len = cats.length, i;
-		for (i = 0; i < len; i++) {
-			if (cats[i].name === catName) {
-				return cats[i];
+		cats: [
+			{
+				name: 'Mojo',
+				clicks: 0,
+				image: 'images/cat1.jpg'
+			},
+			{
+				name: 'Stalin',
+				clicks: 0,
+				image: 'images/cat2.jpg'
+			},
+			{
+				name: 'Lenin',
+				clicks: 0,
+				image: 'images/cat3.jpg'
 			}
+		],
+		getAll: function() {
+			return this.cats;
 		}
-	}
+	};
 
-	function loadCat(cat) {
-		$catName.text(cat.name);
-		$catClicks.text(cat.clicks);
-		$catPics.attr("src", cat.image);
-	}
+	var octopus = {
+		init: function() {
+			model.init(model.cats[0]);
 
-	loadCat(cats[0]);
+			listView.init();
+			mainView.init();
+		},
+		getAll: function() {
+			return model.getAll();
+		},
+		getCurrent: function() {
+			return model.currentCat;
+		},
+		setCurrent: function(cat) {
+			model.currentCat = cat;
+		},
+		loadCurrent: function() {
+			mainView.render(octopus.getCurrent());
+		},
+		incrementCounter: function() {
+			model.currentCat.clicks++;
+		}
+	};
+
+	var listView = {
+		init: function() {
+			this.cats = octopus.getAll();
+			listView.render();
+			this.cats.forEach(function(cat) {
+				$('#' + cat.name).click(function() {	
+
+					octopus.setCurrent(cat);
+					octopus.loadCurrent();
 	
+					return false;
+				});
+			});
+		},
+		render: function() {
+			this.cats.forEach(function(cat) {
+				$('#cat-list').append('<li><button id="' + cat.name + '">' + cat.name + '</button></li>');
+			});
+		}
+	};
+
+	var mainView = {
+		init: function() {
+			this.$catName = $('#cat-name');
+			this.$catClicks = $('#cat-clicks');
+			this.$catPics = $('.cat-pics');
+
+			this.$catPics.click(function() {
+				octopus.incrementCounter();
+				mainView.render(octopus.getCurrent());
+				return false;
+			});
+
+			mainView.render(octopus.getCurrent());
+		},
+		render: function(cat) {
+			this.$catName.text(cat.name);
+			this.$catClicks.text(cat.clicks);
+			this.$catPics.attr("src", cat.image);
+
+		}
+	};
+
+	octopus.init();
 });
 
